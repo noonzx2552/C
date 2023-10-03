c4665993016fb47d254b7a7e2915a1c1049d56bb
noonzx2552 <109357368+noonzx2552@users.noreply.github.com>
2023-10-03 17:44:19 +0700

    Update c2.py

--- c2.py
+++ c2.py
@@ -1,8 +1,9 @@
 r = int(input("radius:"))
 rpm = int(input("RPM:"))
 
-r_inch = 2*r*22/ 7 
-cmpermin = r_inch * 2.54 * rpm
-kmperhr = 0.0006 * cmpermin
+r_inch = 2*r*22/7 
+cm = r_inch * 2.54 * rpm
+km = cm / 100000
+kmperhr = km * 60
     
 print(kmperhr)
